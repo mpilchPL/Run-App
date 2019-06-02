22ae3c29431676b6683d291f9cba5034b8275125
DrazekKD <drazek3850@gmail.com>
2019-06-02 15:10:25 +0200

    added user info

---
+++ Running-App-commitSlicing-074fc81882e4b336ea1cade30be2138e2ffc6f23/app/src/main/java/com/example/runapp/UserInfo.java
@@ -0,0 +1,16 @@
+package com.example.runapp;
+
+public class UserInfo {
+
+    private long runs;
+
+    public UserInfo() { }
+
+    public long getRuns() {
+        return runs;
+    }
+
+    public void setRuns(long runs) {
+        this.runs = runs;
+    }
+}
